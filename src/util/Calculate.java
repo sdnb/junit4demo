4f565921af4feca95e8b7b38f2bee5f77130c989
sdnb <xgct0505@163.com>
2015-07-05 14:10:40 +0800

    首次提交到本地仓库

---
+++ src/util/Calculate.java
@@ -0,0 +1,24 @@
+package util;
+
+public class Calculate {
+		public int  add( int a,int b){
+			int c=a+b;
+			return c;
+			
+		}
+		public int  multiply( int a,int b){
+			int c=a*b;
+			return c;
+			
+		}
+		public int  substract( int a,int b){
+			int c=a-b;
+			return c;
+			
+		}
+		public int  divide( int a,int b){
+			int c=a/b;
+			return c;
+			
+		}
+}
